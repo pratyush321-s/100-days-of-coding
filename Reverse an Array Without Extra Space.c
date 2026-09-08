741d1c222c358fa7dc70459a51d0b39e0cf12910
Pratyush Singhal <pratyush.36624@stu.upes.ac.in>
2026-09-09 00:23:43 +0530

    DAY-31

---
+++ Reverse an Array Without Extra Space.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, temp;
+
+    scanf("%d", &n);
+
+    int a[n];
+
+    // Input array
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    // Reverse the array
+    for(i = 0; i < n / 2; i++)
+    {
+        temp = a[i];
+        a[i] = a[n - 1 - i];
+        a[n - 1 - i] = temp;
+    }
+
+    // Print reversed array
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
