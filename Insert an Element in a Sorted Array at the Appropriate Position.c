eeec5197bf17834fc09ba61e82901d09cb5585a4
Pratyush Singhal <pratyush.36624@stu.upes.ac.in>
2026-09-15 19:30:25 +0530

    DAY-34

---
+++ Insert an Element in a Sorted Array at the Appropriate Position.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int n, element;
+
+    scanf("%d", &n);
+
+    int arr[n + 1];
+
+    // Input sorted array
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Element to insert
+    scanf("%d", &element);
+
+    // Find the appropriate position
+    int pos = 0;
+    while (pos < n && arr[pos] < element) {
+        pos++;
+    }
+
+    // Shift elements to the right
+    for (int i = n; i > pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    // Insert the element
+    arr[pos] = element;
+
+    // Print the sorted array
+    for (int i = 0; i <= n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
---
+++ Insert an Element in an Array at a Given Position.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n, pos, element;
+
+    scanf("%d", &n);
+
+    int arr[n + 1];
+
+    // Input array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Input position and element
+    scanf("%d %d", &pos, &element);
+
+    // Shift elements to the right
+    for (int i = n; i > pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    // Insert element at given position
+    arr[pos] = element;
+
+    // Print the array
+    for (int i = 0; i <= n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
