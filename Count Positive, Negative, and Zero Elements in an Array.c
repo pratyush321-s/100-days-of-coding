092f9d513111712435b2e85e1d1e222f5cc69b01
Pratyush Singhal <pratyush.36624@stu.upes.ac.in>
2026-09-15 14:24:44 +0530

    DAY-30

---
+++ Count Positive, Negative, and Zero Elements in an Array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int positive = 0, negative = 0, zero = 0;
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if (arr[i] > 0)
+            positive++;
+        else if (arr[i] < 0)
+            negative++;
+        else
+            zero++;
+    }
+
+    printf("Positive=%d, Negative=%d, Zero=%d", positive, negative, zero);
+
+    return 0;
+}
